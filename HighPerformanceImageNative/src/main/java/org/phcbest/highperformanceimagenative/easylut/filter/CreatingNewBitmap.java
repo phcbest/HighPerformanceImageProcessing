fe6e5059475b56c420345659616649ac5e8c64ec
phcbest <phcbest2017@outlook.com>
2023-06-25 23:06:40 +0800

    还原误删的lut-ndk实现

--- HighPerformanceImageNative/src/main/java/org/phcbest/highperformanceimagenative/easylut/filter/CreatingNewBitmap.java
+++ HighPerformanceImageNative/src/main/java/org/phcbest/highperformanceimagenative/easylut/filter/CreatingNewBitmap.java
@@ -9,12 +9,12 @@ import org.phcbest.highperformanceimagenative.easylut.lutimage.LUTImage;
 public class CreatingNewBitmap implements BitmapStrategy {
     @Override
     public Bitmap applyLut(Bitmap src, LUTImage lutImage) {
+        long startTime = System.currentTimeMillis();
         int mWidth = src.getWidth();
         int mHeight = src.getHeight();
         int[] pix = new int[mWidth * mHeight];
         src.getPixels(pix, 0, mWidth, 0, 0, mWidth, mHeight);
 
-        long startTime = System.currentTimeMillis();
         for (int y = 0; y < mHeight; y++) {
             for (int x = 0; x < mWidth; x++) {
                 int index = y * mWidth + x;
@@ -25,7 +25,7 @@ public class CreatingNewBitmap implements BitmapStrategy {
         }
         Bitmap bm = Bitmap.createBitmap(mWidth, mHeight, src.getConfig());
         bm.setPixels(pix, 0, mWidth, 0, 0, mWidth, mHeight);
-        Log.i(TAG, "applyLut: 计算时长" + (System.currentTimeMillis() - startTime));
+        Log.i(TAG, "applyLut: TimeConsuming" + (System.currentTimeMillis() - startTime));
         return bm;
     }
 }
